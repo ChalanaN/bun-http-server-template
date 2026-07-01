ffb70b829b0abd74aa6e395479fee2852e1c882f
Chalana Nethsara <wchalanaw@gmail.com>
2026-07-01 17:03:20 +0530

    return status codes

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ import * as path from "node:path"
 const STATIC_DIR = path.resolve(__dirname, process.env.STATIC_DIR || "public")
 
 const sendFile = (filepath: string, options?: { statusCode?: number }): Response => {
-    filepath = decodeURIComponent(filepath.slice(1, filepath.length))
-    let resolvedPath = path.resolve(STATIC_DIR, filepath)
+    filepath = decodeURIComponent(filepath)
+    let resolvedPath = path.resolve(STATIC_DIR, "." + filepath)
 
-    if (!resolvedPath.startsWith(STATIC_DIR)) return new Response("403")
+    if (!resolvedPath.startsWith(STATIC_DIR + path.sep)) return new Response("403", { status: 403 })
 
     return new Response(Bun.file(resolvedPath), {
         status: options?.statusCode
@@ -17,19 +17,23 @@ Bun.serve({
     port: process.env.PORT || 80,
 
     async fetch(req) {
-        const requestURL = new URL(req.url)
+        switch (req.method) {
+            case "GET":
+                const requestURL = new URL(req.url)
 
-        console.log(requestURL.pathname)
+                return sendFile(requestURL.pathname)
 
-        return sendFile(requestURL.pathname)
+            default:
+                return new Response("405", { status: 405 })
+        }
     },
 
     error(err) {
         if (err.code == "ENOENT") {
-            return new Response("404")
+            return new Response("404", { status: 404 })
         } else {
             console.log(err)
-            return new Response("500")
+            return new Response("500", { status: 500 })
         }
     }
 })
